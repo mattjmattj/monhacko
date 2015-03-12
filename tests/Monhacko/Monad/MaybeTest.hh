546624e2d7e8f27ca336e0c30815a7636c438c95
Matthias Jouan <matthias.jouan@gmail.com>
2015-03-12 22:36:30 +0000

    Rewrite of tests from phpunit to hackunit

--- tests/Monhacko/Monad/MaybeTest.hh
+++ tests/Monhacko/Monad/MaybeTest.hh
@@ -1,8 +1,10 @@
-<?hh //partial
+<?hh //strict
 
 namespace Monhacko\Test;
 
-use \PHPUnit_Framework_TestCase as TestCase;
+//use \PHPUnit_Framework_TestCase as TestCase;
+
+use HackPack\HackUnit\Core\TestCase;
 
 use \Monhacko\Monad\Maybe;
 
@@ -10,22 +12,22 @@ class MaybeTest extends TestCase {
 	
 	public function testJust () : void {
 		
-		$this->assertEquals(2, Maybe::just(2)->value());
-		$this->assertEquals("foobar", Maybe::just("foobar")->value());
+		$this->expect(Maybe::just(2)->value())->toEqual(2);
+		$this->expect(Maybe::just("foobar")->value())->toEqual("foobar");
 		
 		$maybe = Maybe::just(2);
 		$result = $maybe->bind($x ==> Maybe::just($x * 2))
 						->bind($x ==> Maybe::just($x * 3))
 						->value();
 				
-		$this->assertEquals(12, $result);
+		$this->expect($result)->toEqual(12);
 		
 		$maybe = Maybe::just("foo");
 		$result = $maybe->bind( $s ==> Maybe::just(strlen($s)))
 						->bind( $x ==> Maybe::just($x *2))
 						->value();
 						
-		$this->assertEquals(6, $result);
+		$this->expect($result)->toEqual(6);
 	}
 	
 	public function testNothing () : void {
@@ -35,7 +37,7 @@ class MaybeTest extends TestCase {
 						->bind($x ==> Maybe::just($x * 3))
 						->value();
 				
-		$this->assertEquals(null, $result);
+		$this->expect($result)->toEqual(null);
 	}
 	
 	public function testMaybe () : void {
@@ -43,8 +45,8 @@ class MaybeTest extends TestCase {
 		$maybe = Maybe::just(2);
 		$result = $maybe->bind($x ==> ($x>10) ? Maybe::just(5) : Maybe::nothing());
 		
-		$this->assertEquals(Maybe::nothing(), $result);
-		$this->assertEquals(null, $result->value());
+		$this->expect($result)->toEqual(Maybe::nothing());
+		$this->expect($result->value())->toEqual(null);
 	}
 	
 	public function testThen () : void {
@@ -62,6 +64,6 @@ class MaybeTest extends TestCase {
 		
 		$maybe->then($fn(1,$buffer))->then($fn(2,$buffer));
 		
-		$this->assertEquals([1,2], $buffer);
+		$this->expect($buffer)->toEqual([1,2]);
 	}
 }
--- tests/Monhacko/Monad/OutTest.hh
+++ tests/Monhacko/Monad/OutTest.hh
@@ -1,8 +1,10 @@
-<?hh //partial
+<?hh //strict
 
 namespace Monhacko\Test;
 
-use \PHPUnit_Framework_TestCase as TestCase;
+//use \PHPUnit_Framework_TestCase as TestCase;
+
+use HackPack\HackUnit\Core\TestCase;
 
 use \Monhacko\Monad\Out;
 use \Monhacko\Monad\Maybe;
@@ -11,17 +13,18 @@ class OutTest extends TestCase {
 	
 	public function testBind () : void {
 		
-		$this->expectOutputString('foo***foo');
-		
-		Maybe::just('foo')
-			->bind($x ==> Out::write($x))
-			->bind($x ==> Out::write("***$x"));
+		$this->expectCallable(function() {
+			Maybe::just('foo')
+				->bind($x ==> Out::write($x))
+				->bind($x ==> Out::write("***$x"));
+		}) ->toOutputString('foo***foo');
 	}
 	
 	public function testThen () : void {
-		$this->expectOutputString('foo***foo');
 		
-		Out::write('foo')
-			->then(() ==> Out::write("***foo"));
+		$this->expectCallable(function() {
+			Out::write('foo')
+				->then(() ==> Out::write("***foo"));
+		}) ->toOutputString('foo***foo');
 	}
 }
